c189dc0f813a185690f6a9d86932ada24fc4e0c8
Eren Ertem <mail.erenertem@gmail.com>
2025-04-02 16:41:52 +0300

    initialize spring boot

---
+++ src/main/java/ertem/eren/aibuster/AiBusterApplication.java
@@ -0,0 +1,13 @@
+package ertem.eren.aibuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBusterApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(AiBusterApplication.class, args);
+  }
+  
+}
